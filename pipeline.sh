91b22d3c772fc576caf7cc96eb05b3b8f84123a2
Alex Mikhalev <alex.mikhalev@gmail.com>
2020-05-17 01:59:47 +0100

    Fixed spacy gears and added local intake and pipeline to help testing

---
+++ pipeline.sh
@@ -0,0 +1,3 @@
+python RedisIntakeLocal.py 
+gears-cli --host 127.0.0.1 lang_detect_gears_paragraphs.py --requirements requirements_gears.txt
+gears-cli --host 127.0.0.1 spacy_sentences_geared.py --requirements requirements_gears_spacy.txt
--- spacy_sentences_geared.py
+++ spacy_sentences_geared.py
@@ -1,21 +1,25 @@
 import spacy 
 nlp=spacy.load('en_core_web_md', disable=['ner','tagger'])
 
+def remove_prefix(text, prefix):
+    return text[text.startswith(prefix) and len(prefix):]
+
 def parse_paragraphs(x):
+    key_prefix="paragraphs:"
     #detect language of the article
     lang=execute('GET', 'lang_article:' + x['key'])
     if lang=='en':
         paragraphs =x['value']
         doc=nlp(paragraphs)
         idx=1
-        article_id=x['key']
+        article_id=remove_prefix(x['key'],key_prefix)
         for each_sent in doc.sents:
             sentence_key=f"tokens:{article_id}:{idx}"
             execute('SET', sentence_key, each_sent)
             idx+=1
         execute('SADD','processed_docs_stage2_sentence', article_id)
     else:
-        execute('SADD','articles_to_delete', x['key'])
+        execute('SADD','screw_ups', x['key'])
     
 
 gb = GB()
