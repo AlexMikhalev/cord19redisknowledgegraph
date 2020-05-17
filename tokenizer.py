ed87a78f358fab23fa81788eb2c40a6273b7f57c
Gavin D'mello <dmellogavin5000@gmail.com>
2020-05-17 19:32:57 +0100

    Updated gear

--- tokenizer.py
+++ tokenizer.py
@@ -1,22 +1,16 @@
 import torch
 from transformers import AutoTokenizer, AutoModel
-from gearsclient import GearsRemoteBuilder as GearsBuilder
-import redis
-from gearsclient import execute
-# conn = redis.Redis(host='10.144.211.47', port=6379)
-conn = redis.Redis(host='127.0.0.1', port=6379)
 
 tokenizer = AutoTokenizer.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
 model = AutoModel.from_pretrained("emilyalsentzer/Bio_ClinicalBERT")
 
-def parse_para(sentences_ids):
-	sentences = []
-	for sentence_id in sentences_ids:
-		sentences.append({
-			'sentence': execute('get', sentence_id),
-			'sentence_id' : sentence_id.split(':')[1]
-		})
-	return sentences
+def parse_para(sentence_meta):
+	tokens = tokenizer.tokenize(sentence_meta['value'])
+	push_list({
+		'sentence': sentence_meta['value'],
+		'sentence_id' : sentence_meta['key'].split(':')[1],
+		'tokenized': tokens
+	})
 
 def push_list(meta):
 	tokens = list(meta['tokenized'])
@@ -24,11 +18,8 @@ def push_list(meta):
 	for token in meta['tokenized']:
 		execute('lpush', key, token)
 
-res = GearsBuilder('KeysOnlyReader', r=conn).\
-	  map(lambda x:execute('keys', 'paragraphs:*')).\
-	  flatmap(lambda x: parse_para(x)).\
-	  flatmap(lambda x:  {'tokenized' : tokenizer.tokenize(x['sentence']), 'sentence_id':x['sentence_id']}).\
-	  map(lambda x: push_list(x)).\
-	  run()
+gb = GB()
+gb.foreach(parse_para)
+gb.run('paragraphs:*')
 
-print(res)
+print(gb)
