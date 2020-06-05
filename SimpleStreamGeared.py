f94b1e4462ef02d715a4718ae2aff98b802f39a7
Alex Mikhalev <alex.mikhalev@gmail.com>
2020-06-05 06:15:15 +0100

    Added docs

--- SimpleStreamGeared.py
+++
@@ -1,9 +0,0 @@
-def log_stream(x):
-    log("My cluster %s" % hashtag())
-    log("Test log Steam Key %s" % x['key'])
-    log("Test log article id %s " % x['value']['sentence_key'])
-    log("Test log content %s " % x['value']['content'][:100])
-
-bg = GearsBuilder('StreamReader')
-bg.foreach(log_stream)
-bg.register('sentence_to_tokenise_*', batch=1, mode="async_local", onFailedPolicy='continue', trimStream=True)
--- demo_script.sh
+++
@@ -1,9 +0,0 @@
-SECONDS=0
-python RedisIntakeRedisClusterSample.py 
-echo "RedisIntakeRedisClusterSample.py finished in $SECONDS seconds."
-SECONDS=0
-python matcher_node_hash.py 
-echo "matcher_node_hash.py finished in $SECONDS seconds."
-SECONDS=0
-python matcher_node.py 
-echo "matcher_node.py finished in $SECONDS seconds."
